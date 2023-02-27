e81aeb8881706888c9dee5f24311a975c42e5708
madariana <madarianar@gmail.com>
2023-02-27 09:27:46 -0600

    Se agregan archivos

---
+++ JAVA05/java02/hola-sts/src/clases/HolaJava.java
@@ -0,0 +1,8 @@
+package clases;
+
+
+public class HolaJava {
+	void saludo() {
+		System.out.println("Hola mundo desde el IDE, basado en eclipse");
+	}
+}
---
+++ JAVA05/java02/hola-sts/src/principal/EjecutarHolaJava.java
@@ -0,0 +1,16 @@
+package principal;
+import clases.HolaJava;
+
+public class EjecutarHolaJava {
+
+	public static void main(String[] mariana) {
+		// TODO Auto-generated method stub
+		HolaJava objHJ;
+		objHJ = new HolaJava();
+		objHJ.saludo ();
+		HolaJava objHJ1 = new HolaJava();
+		objHJ1.saludo();
+
+	}
+
+}
