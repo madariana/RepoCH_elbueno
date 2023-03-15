169a6066f77afbc4c4f0a9db7d80075d48088197
madariana <madarianar@gmail.com>
2023-03-15 17:04:03 -0600

    Se agregan archivos

---
+++ JAVA05/java12/pruebasJunit/src/pruebasJunitmm/app/Calculadora.java
@@ -0,0 +1,17 @@
+package pruebasJunitmm.app;
+
+public class Calculadora {
+
+	public int sumar (int n1, int n2) {
+		return (n1+n2);
+	}
+	
+	public int restar(int n1, int n2) {
+		return (n1-n2);
+	}
+	
+	public double division(double d1, double d2) {
+		return d1/d2;
+				
+	}
+}
